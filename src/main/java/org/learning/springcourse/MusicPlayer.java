273ef141e72780e07dea363558dd4ef2fc39717c
Eduard <eduardyacenko@gmail.com>
2023-01-11 10:38:21 +0200

    Added annotation @PostConstruct and @PreDestroy

--- src/main/java/org/learning/springcourse/MusicPlayer.java
+++ src/main/java/org/learning/springcourse/MusicPlayer.java
@@ -1,19 +1,35 @@
 package org.learning.springcourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class MusicPlayer {
 
-    private Music music1;
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int volume;
 
+    private Music music1;
     private Music music2;
 
-    public MusicPlayer(Music rockMusic, Music classicalMusic) {
-        this.music1 = rockMusic;
-        this.music2 = classicalMusic;
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    @Autowired
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
