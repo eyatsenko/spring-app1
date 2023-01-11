94a74c3cced3c4e66997964a39b9af85a4c17ddd
Eduard <eduardyacenko@gmail.com>
2023-01-11 09:58:16 +0200

    Added dependency injection using Autowired

--- src/main/java/org/learning/springcourse/MusicPlayer.java
+++ src/main/java/org/learning/springcourse/MusicPlayer.java
@@ -1,21 +1,22 @@
 package org.learning.springcourse;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class MusicPlayer {
-    private final ClassicalMusic classicalMusic;
-    private final RockMusic rockMusic;
 
-    @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    private Music music1;
+
+    private Music music2;
+
+    public MusicPlayer(Music rockMusic, Music classicalMusic) {
+        this.music1 = rockMusic;
+        this.music2 = classicalMusic;
     }
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music1.getSong() + ", " + music2.getSong();
     }
 }
