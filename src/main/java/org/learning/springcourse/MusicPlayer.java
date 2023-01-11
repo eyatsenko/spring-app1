8a37588a9797204a8387184cb21345071b47e691
Eduard <eduardyacenko@gmail.com>
2023-01-11 09:39:39 +0200

    Added dependency injection using Autowired

--- src/main/java/org/learning/springcourse/MusicPlayer.java
+++ src/main/java/org/learning/springcourse/MusicPlayer.java
@@ -1,54 +1,21 @@
 package org.learning.springcourse;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class MusicPlayer {
-//    private List<Music> playMusicList = new ArrayList<>();
-    private Music music;
-    private String name;
-    private int volume;
+    private final ClassicalMusic classicalMusic;
+    private final RockMusic rockMusic;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public Music getMusic() {
-        return music;
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    //    public List<Music> getPlayMusicList() {
-//        return playMusicList;
-//    }
-
-//    public void setPlayMusicList(List<Music> playMusicList) {
-//        this.playMusicList = playMusicList;
-//    }
-
-    public void playMusic() {
-//        for (Music music : musicList) {
-//            System.out.println("Playing: " + music.getSong());
-//        }
-        System.out.println("Playing: " + music.getSong());
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong();
     }
 }
